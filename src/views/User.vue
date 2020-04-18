d10fc5e9bf9982d612dd9f2dfc84d56270718061
UbaldoRosas <liadz.orb@gmail.com>
2020-04-17 23:57:33 -0500

    add dynamic routes

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,12 @@
 <template>
-  <h1>Show event #{{ id }}</h1>
+  <div>
+    <h1>Usuario: {{ username }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['id']
+  props: ['username']
 }
 </script>
 
